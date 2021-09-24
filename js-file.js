92220423d2ea7d02c2aebb0f600dfab532a83d71
mbsakil99 <87930206+mbsakil99@users.noreply.github.com>
2021-09-24 19:38:25 +0600

    All File added

---
+++ js-file.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = "Inner HTML change";
+
+function myFunction(){
+    document.getElementById('demo2').style.fontSize='25px';
+}
+
+
+function myFunction2() {
+    document.getElementById ("demo4").innerHTML ="Hello Bangladesh";
+}
